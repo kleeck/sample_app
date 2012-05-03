5ebd2c5f10572afd45ab316e319960839809eca0
Steve <kleecksj@gmail.com>
2012-05-02 22:09:54 -0500

    Fixed an issue I caused in the application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
 	# Return a title on a per-page basis.
 	def title
-		base_title = "Ruby on Rails Tutorial Sample App"
+		base_title = "RoR3Tutorial: Sample App"
 		if @title.nil?
 			base.title
 		else
-			"#{base_title} | @{title}"
+			"#{base_title} | #{@title}"
 		end
 	end	
 end
